0960480fa73c8cf53187636389ead98896b9d444
Benjamin Donnelly <lbd@google.com>
2025-01-08 12:46:36 -0800

    Small error fix in service worker

--- chrome/service_worker.js
+++ chrome/service_worker.js
@@ -1065,7 +1065,7 @@ background.displayPasswordWarningIfNeeded_ = function (url, email, tabId) {
 background.displayPhishingWarningIfNeeded_ = function (tabId, request) {
     chrome.storage.local.get(
         background.PHISHING_WARNING_ALLOWLIST_KEY_, function (result) {
-            let u = new URL(url);
+            let u = new URL(request.url);
             const currentHost = u.origin;
             const phishingWarningAllowlist =
                 result[background.PHISHING_WARNING_ALLOWLIST_KEY_];
